26029e782526bbbe82d6bb8d6c758f1b04e465f3
SparXFusion <s2234nadar@gmail.com>
2021-10-08 11:26:56 +0530

    lavender: switch to `SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS`
    * BOARD_PLAT_PRIVATE_SEPOLICY_DIR is deprecated in Android 12
    
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,8 +158,7 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/configs/seccomp
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true

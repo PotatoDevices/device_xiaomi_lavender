71fcc8b58e500aedd863ccdd02df219d8ea12f87
wHo-EM-i <84293429+wHo-EM-i@users.noreply.github.com>
2021-11-14 15:21:08 +0530

    lavender: Build extphone libraries
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- device.mk
+++ device.mk
@@ -410,6 +410,10 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
